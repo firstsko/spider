f7bb962d58444f032a2baaed74b96005cf29fab4
frost <simple1115@gmail.com>
2015-04-16 02:24:37 +0800

    loglevel

--- log.h
+++ log.h
@@ -1,5 +1,18 @@
 #ifndef _LOG_H_
 #define _LOG_H_
 
+typedef enum {
+	LOG_EMERG = 0,
+	LOG_ALERT,
+	LOG_CRIT,
+	LOG_ERR,
+	LOG_WARNING,
+	LOG_NOTICE,
+	LOG_INFO,
+	LOG_DEBUG,
+} Loglevel;
+
+
+
 
 #endif 
