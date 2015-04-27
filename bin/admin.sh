402713fd8498a739eaf9c7026785e9fefe482260
frost <simple1115@gmail.com>
2015-04-27 10:06:20 +0800

    controller script ok

--- bin/admin.sh
+++ bin/admin.sh
@@ -1,14 +1,60 @@
 #!/bin/bash
 
-if [ $# -lt 2 ];
+if [ $# -lt 1 ];
 then
 	echo usage $0 "start or stop"
 	exit 1
-
 fi
 
-option=$1
+if [ $1 = "start" ]
+then
+	pidnum=`ps -ef|grep "\./server"|grep -v grep|wc -l`
+	if [ $pidnum -lt 1 ]
+	then
+        ./server 8888
+	else
+		for pid in `ps -ef|grep "./server"|grep -v grep|awk '{print $2}'`
+		do
+			target_exe=`readlink /proc/$pid/exe | awk '{print $1}'`
+			if [ -n "$target_exe" ]
+			then
+				local_exe=`pwd`"/server"
+				if [ $target_exe == $local_exe ]
+				then
+					echo "program already started."
+					exit
+				else
+        			./server 8888
+				fi
+			else
+        			./server 8888
+				break
+			fi	
+		done
+	fi
+fi
 
-if [ $option = "start" ]
-
+if [ $1 = "stop" ]
+then
+	pidnum=`ps -ef|grep "./server"|grep -v grep|wc -l`
+	if [ $pidnum -lt 1 ]
+	then
+		echo "no program killed."
+	else
+		for pid in `ps -ef|grep "./server"|grep -v grep|awk '{print $2}'`
+		do
+			target_exe=`readlink /proc/$pid/exe | awk '{print $1}'`
+			#如果target_exe非空字符串
+			if [ -n "$target_exe" ]
+			then
+				local_exe=`pwd`"/server"
+				if [ $target_exe == $local_exe ]
+				then
+					kill -10 $pid
+				fi
+			fi
+		done
+		echo "server stopped."
+	fi
+fi
 
