22d47bbb441ec322ac1f2193069c787f286c03f4
frost <simple1115@gmail.com>
2015-04-30 01:20:28 +0800

    Makefile order corrected, link error soloved

--- test/main.cpp
+++ test/main.cpp
@@ -8,6 +8,8 @@
 #include <netinet/tcp.h> 
 #include <string.h>
 #include <strings.h>
+#include <google/protobuf/message.h>
+#include <google/protobuf/descriptor.h>
 
 #include "message.pb.h"
 
@@ -17,6 +19,21 @@ typedef struct {
 	unsigned length;
 	unsigned message_id; 
 } header;
+
+google::protobuf::Message* CreateMessage(const std::string &name) {
+	google::protobuf::Message* message = NULL;
+	const google::protobuf::Descriptor* descriptor =
+		google::protobuf::DescriptorPool::generated_pool()->FindMessageTypeByName(name);
+	if (descriptor) {
+		const google::protobuf::Message* prototype =
+			google::protobuf::MessageFactory::generated_factory()->GetPrototype(descriptor);
+		if (prototype) {
+			message = prototype->New();
+		}
+	}
+
+	return message;
+}
 
 int main(int argc, char **argv) {
 	if (argc != 3) {
