1cb7d27fa07760094f3a47525b73547ffffebcf7
chensui <chensui@jd.com>
2015-05-12 02:15:54 +0800

    http url path % unsolved

--- src/http_url.cpp
+++ src/http_url.cpp
@@ -5,15 +5,31 @@
 using namespace std;
 
 int HttpUrl::Parse(const string &source) {  
+	// protocol + hostname + port + path + querystring = url
 	size_t size = source.length();
 
-	const string proto_regex = "^https?//";
-
-	const string domain_regex = "";
+	const string proto_regex = "^(https?:\\/\\/)?";
+	const string hostname_regex = "([0-9a-zA-Z-]+\\.)+[0-9a-zA-Z]{2,6}";
+	const string port_regex = "(:[0-9]{1,5})?";
+	const string path_regex = "(\\/([\\w]+\\/)+[\\w])?";
+	const string querystring_regex = "\\?((([\\w|%]+=[\\w|%]+)&)([\\w|%]+=[\\w|%]))?$";
 	
-		
+	char buf[1024] = {0};
+	snprintf(buf, sizeof(buf) - 1, "%s%s%s%s%s", proto_regex.c_str(), hostname_regex.c_str(), port_regex.c_str(), path_regex.c_str(), querystring_regex.c_str());
+	string valid_http_url = buf;
 
+	regex_t reg;
+	if (regcomp(&reg, valid_http_url.c_str(), 0) != 0) {
+		printf ("Failed to Compile Regular Expression\n");
+		regfree(&reg);
+		return -1;
+	}
 
+	// Not A Http Url
+	if (regexec(&reg, source.c_str(), 0, NULL, 0) != 0) {
+		regfree(&reg);
+		return -1;
+	}
 
     return 0;  
 }
--- src/unit_test.cc
+++ src/unit_test.cc
@@ -7,6 +7,8 @@
 #include <execinfo.h>
 //include "foo.h"
 
+using namespace std;
+
 void print_trace(int signal) {
 	// Flush Log
 //	Log::Instance()->Flush();
@@ -30,17 +32,28 @@ void print_trace(int signal) {
 	free(symbols);
 	exit(EXIT_FAILURE);
 }
-
+/*
 int hello() {
 	char b[3] = {'a', 'b', 'c'};
 	int *a = (int *) 0x00000001;
 	printf("%d%c\n", *a, b[100]);
 
 	return 0;
-}
+}*/
 
 int main () {
-	signal(SIGSEGV, print_trace);
-	hello();
+	//signal(SIGSEGV, print_trace);
+//	hello();
+	const string proto_regex = "^(https?:\\/\\/)?";
+	const string hostname_regex = "([0-9a-zA-Z-]+\\.)+[0-9a-zA-Z]{2,6}";
+	const string port_regex = "(:[0-9]{1,5})?";
+	const string path_regex = "(\\/([\\w]+\\/)+[\\w])?";
+	const string querystring_regex = "\\?((([\\w|%]+=[\\w|%]+)&)([\\w|%]+=[\\w|%]))?$";
+	
+	char buf[1024] = {0};
+	snprintf(buf, sizeof(buf) - 1, "%s%s%s%s%s", proto_regex.c_str(), hostname_regex.c_str(), port_regex.c_str(), path_regex.c_str(), querystring_regex.c_str());
+	string valid_http_url = buf;
+
+	printf("regex: %s\n", valid_http_url.c_str());
 	return 0;
 }
